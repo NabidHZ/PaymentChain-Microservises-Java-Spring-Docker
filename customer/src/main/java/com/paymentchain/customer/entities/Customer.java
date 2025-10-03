e571a253800ed3bdd70693bc90f9b8ccc24301ad
NabidHZ <nabidhassan34@gmail.com>
2025-10-03 23:17:39 +0200

    Repositorio customer

--- customer/src/main/java/com/paymentchain/customer/entities/Customer.java
+++ customer/src/main/java/com/paymentchain/customer/entities/Customer.java
@@ -3,14 +3,17 @@ package com.paymentchain.customer.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 
+@Data
 @Entity
 public class Customer {
 
-    @Data
+
     @GeneratedValue(strategy = GenerationType.AUTO) //Esto va ahcer que que "id" sea autoincremental
+    @Id
     private long id;
     private String name;
     private int phone;
--- customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
+++ customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
@@ -5,7 +5,7 @@ import com.paymentchain.customer.entities.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> { //Utiliza long porque la primarikey de Customr es long
 
-    //Estar interfaz me permite guradar y mandar datos a la base de datos
+    //Estar interfaz me permite guradar y mandar datos , sin la necesidad de escribir esos metoddos
 
-
+    //Dentro de JpaRepository ya estan los metodos  CRUD que seran heredados por esta interfaz y los que usen esta interfaz
 }
